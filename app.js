4924a3052fefc5dcd1c6bf7448bd4e76a8fe57f7
Peter Opeyemi <92626192+PeterAdedokun1@users.noreply.github.com>
2022-04-26 19:12:48 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,96 @@
+// DOM ELEMENT
+const resultEl = document.getElementById("result")
+const lengthEl = document.getElementById("length")
+const upperCaseEl = document.getElementById("uppercase")
+const lowerCaseEl = document.getElementById("lowercase")
+const numbersEl = document.getElementById("numbers")
+const symbolsEl = document.getElementById("symbols")
+const generateEl = document.getElementById("generate")
+const clipBoardEl = document.getElementById("clipboard")
+
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+}
+
+generateEl.addEventListener("click", () => {
+    const length = Number(lengthEl.value)
+    const hasLower = lowerCaseEl.checked
+    const hasUpper = upperCaseEl.checked
+    const hasNumber = numbersEl.checked
+    const hasSymbol = symbolsEl.checked
+    
+
+    resultEl.innerHTML = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+})
+
+//Copy password to clipBoard
+clipBoardEl.addEventListener("click", () => {
+    const textArea = document.createElement("textarea");
+    const password = resultEl.innerHTML;
+    if (!password) {
+        return;
+    }
+    textArea.value = password;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    textArea.remove();
+    alert("Password copied to clipboard!!")
+})
+// generate password function
+const generatePassword = (lower,upper,number,symbol,length) => {
+    let generatedPassword = "";
+    const typesCount = lower + upper + number + symbol;
+   // console.log(typesCount)
+
+
+    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0])
+
+    if (typesCount === 0) {
+        return ""
+    }
+
+    for (let i = 0; i < length; i += typesCount){
+        typesArr.forEach(item => {
+            const funcName = Object.keys(item)[0]; //it gets the random checked buttons 
+            // console.log(funcName)
+                generatedPassword += randomFunc[funcName]()
+            })
+    }
+    const finalPassword = generatedPassword.slice(0, length)
+    return finalPassword
+}       
+
+function getRandomLower() {
+        return String.fromCharCode(Math.floor(Math.random() * 26) +  97)
+}
+function getRandomUpper() {
+        return String.fromCharCode(Math.floor(Math.random() * 26) +  65)
+}
+function getRandomNumber() {
+        return String.fromCharCode(Math.floor(Math.random() * 10) +  48)
+}
+function getRandomSymbol() {
+    const symbol = `!@#$%^&*()_{`;
+    return symbol[Math.floor(Math.random()* symbol.length)]
+}
+
+// console.log(getRandomLower())
+// console.log(getRandomUpper())
+// console.log(getRandomNumber())
+// console.log(getRandomSymbol ())
+// // console.log(Symbol ())
+// console.log("hello")
+
+
+// function Symbol() {
+//     const symbol = `abcdefghijklmoppqrstwxyz`;
+//     return symbol[Math.floor(Math.random()* symbol.length)]
+// }
+
+
+
